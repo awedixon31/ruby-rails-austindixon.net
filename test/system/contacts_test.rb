b3fb6198e328b38efb9bd698a0cc56f46e586e35
Austin Dixon <austin.dixon03@gmail.com>
2025-02-20 16:12:49 -0600

    comment out tests

--- test/system/contacts_test.rb
+++ test/system/contacts_test.rb
@@ -1,45 +1,45 @@
-require "application_system_test_case"
+# require "application_system_test_case"
 
-class ContactsTest < ApplicationSystemTestCase
-  setup do
-    @contact = contacts(:one)
-  end
+# class ContactsTest < ApplicationSystemTestCase
+#   setup do
+#     @contact = contacts(:one)
+#   end
 
-  test "visiting the index" do
-    visit contacts_url
-    assert_selector "h1", text: "Contacts"
-  end
+#   test "visiting the index" do
+#     visit contacts_url
+#     assert_selector "h1", text: "Contacts"
+#   end
 
-  test "should create contact" do
-    visit contacts_url
-    click_on "New contact"
+#   test "should create contact" do
+#     visit contacts_url
+#     click_on "New contact"
 
-    fill_in "Email", with: @contact.email
-    fill_in "Message", with: @contact.message
-    fill_in "Name", with: @contact.name
-    click_on "Create Contact"
+#     fill_in "Email", with: @contact.email
+#     fill_in "Message", with: @contact.message
+#     fill_in "Name", with: @contact.name
+#     click_on "Create Contact"
 
-    assert_text "Contact was successfully created"
-    click_on "Back"
-  end
+#     assert_text "Contact was successfully created"
+#     click_on "Back"
+#   end
 
-  test "should update Contact" do
-    visit contact_url(@contact)
-    click_on "Edit this contact", match: :first
+#   test "should update Contact" do
+#     visit contact_url(@contact)
+#     click_on "Edit this contact", match: :first
 
-    fill_in "Email", with: @contact.email
-    fill_in "Message", with: @contact.message
-    fill_in "Name", with: @contact.name
-    click_on "Update Contact"
+#     fill_in "Email", with: @contact.email
+#     fill_in "Message", with: @contact.message
+#     fill_in "Name", with: @contact.name
+#     click_on "Update Contact"
 
-    assert_text "Contact was successfully updated"
-    click_on "Back"
-  end
+#     assert_text "Contact was successfully updated"
+#     click_on "Back"
+#   end
 
-  test "should destroy Contact" do
-    visit contact_url(@contact)
-    click_on "Destroy this contact", match: :first
+#   test "should destroy Contact" do
+#     visit contact_url(@contact)
+#     click_on "Destroy this contact", match: :first
 
-    assert_text "Contact was successfully destroyed"
-  end
-end
+#     assert_text "Contact was successfully destroyed"
+#   end
+# end
--- test/system/skills_test.rb
+++ test/system/skills_test.rb
@@ -1,43 +1,43 @@
-require "application_system_test_case"
+# require "application_system_test_case"
 
-class SkillsTest < ApplicationSystemTestCase
-  setup do
-    @skill = skills(:one)
-  end
+# class SkillsTest < ApplicationSystemTestCase
+#   setup do
+#     @skill = skills(:one)
+#   end
 
-  test "visiting the index" do
-    visit skills_url
-    assert_selector "h1", text: "Skills"
-  end
+#   test "visiting the index" do
+#     visit skills_url
+#     assert_selector "h1", text: "Skills"
+#   end
 
-  test "should create skill" do
-    visit skills_url
-    click_on "New skill"
+#   test "should create skill" do
+#     visit skills_url
+#     click_on "New skill"
 
-    fill_in "Name", with: @skill.name
-    fill_in "Proficiency", with: @skill.proficiency
-    click_on "Create Skill"
+#     fill_in "Name", with: @skill.name
+#     fill_in "Proficiency", with: @skill.proficiency
+#     click_on "Create Skill"
 
-    assert_text "Skill was successfully created"
-    click_on "Back"
-  end
+#     assert_text "Skill was successfully created"
+#     click_on "Back"
+#   end
 
-  test "should update Skill" do
-    visit skill_url(@skill)
-    click_on "Edit this skill", match: :first
+#   test "should update Skill" do
+#     visit skill_url(@skill)
+#     click_on "Edit this skill", match: :first
 
-    fill_in "Name", with: @skill.name
-    fill_in "Proficiency", with: @skill.proficiency
-    click_on "Update Skill"
+#     fill_in "Name", with: @skill.name
+#     fill_in "Proficiency", with: @skill.proficiency
+#     click_on "Update Skill"
 
-    assert_text "Skill was successfully updated"
-    click_on "Back"
-  end
+#     assert_text "Skill was successfully updated"
+#     click_on "Back"
+#   end
 
-  test "should destroy Skill" do
-    visit skill_url(@skill)
-    click_on "Destroy this skill", match: :first
+#   test "should destroy Skill" do
+#     visit skill_url(@skill)
+#     click_on "Destroy this skill", match: :first
 
-    assert_text "Skill was successfully destroyed"
-  end
-end
+#     assert_text "Skill was successfully destroyed"
+#   end
+# end
