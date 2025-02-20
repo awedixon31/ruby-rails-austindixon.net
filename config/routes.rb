83a31dd30911077c074c165305e01cf99579e971
Austin Dixon <austin.dixon03@gmail.com>
2025-02-20 16:05:42 -0600

    add in working nightscout url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root "home#index"
   resources :projects
   resources :skills
-  resources :contacts, only: [:new, :create]
+  resources :contacts, only: [ :new, :create ]
 end
